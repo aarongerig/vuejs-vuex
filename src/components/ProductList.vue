fa5917f6a8c5c99725e1f2f48c4e78092553ae1b
Aaron Gerig <a.gerig@w-vision.ch>
2019-04-26 17:25:50 +0200

    started working on chapter 4

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,13 +5,15 @@
         <ul v-else>
             <li v-for="product in products" :key="product.id">
                 {{ product.title }} – {{ product.price | currency }} – {{ product.inventory }}
-                <button @click="addProductToCart(product)">Add to cart</button>
+                <button :disabled="!productIsInStock(product)" @click="addProductToCart(product)">Add to cart</button>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'ProductList',
 
@@ -22,20 +24,25 @@ export default {
   },
 
   computed: {
-    products () {
-      return this.$store.getters.availableProducts
-    }
+    ...mapState({
+      products: state => state.products
+    }),
+
+    ...mapGetters({
+      productIsInStock: 'productIsInStock'
+    })
   },
 
   methods: {
-    addProductToCart (product) {
-      this.$store.dispatch('addProductToCart', product)
-    }
+    ...mapActions({
+      fetchProducts: 'fetchProducts',
+      addProductToCart: 'addProductToCart'
+    })
   },
 
   created () {
     this.loading = true
-    this.$store.dispatch('fetchProducts').then(() => {
+    this.fetchProducts().then(() => {
       this.loading = false
     })
   }
